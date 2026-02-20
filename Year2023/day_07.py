8ee5b0d6241b48fa505508ece81aae6c69fd5c9d
Vincent Terpstra <vdterpst@gmail.com>
2026-02-20 17:08:22 -0500

    solution for 2023 day 7 part 2

--- Year2023/day_07.py
+++ Year2023/day_07.py
@@ -27,7 +27,10 @@ class Hand:
         return f'cards: {self.cards}, bid: {self.bid}, {self.rank}, high: {self.high_card}'
 
     def get_rank(self):
-        counts = sorted(Counter(self.cards).values(), reverse=True)
+        return self.get_rank_with_count(sorted(Counter(self.cards).values(), reverse=True))
+
+    @staticmethod
+    def get_rank_with_count(counts):
         match counts:
             case [5]:
                 return Rank.FIVE
@@ -53,8 +56,36 @@ class Hand:
 
 
 hands = list(map(Hand, lines))
-# sort first by the rank then by card value by index
-hands.sort(key=lambda h: (h.rank.value, list(map(Hand.get_card_value, h.cards))))
 
-total = sum(((key + 1) * hand.bid) for key, hand in enumerate(hands))
-print(total)
+
+def solution_1():
+    # sort first by the rank then by card value by index
+    hands.sort(key=lambda h: (h.rank.value, list(map(Hand.get_card_value, h.cards))))
+
+    total = sum(((key + 1) * hand.bid) for key, hand in enumerate(hands))
+    print(total)
+
+
+def solution_2():
+    def get_card_value_joker(card):
+        lookup = {'A': 14, 'K': 13, 'Q': 12, 'J': 1, 'T': 10}
+        return lookup[card] if card in lookup else int(card)
+
+    def get_rank_joker(hand):
+        jokers = sum(1 for c in hand.cards if c == 'J')
+        counts = Counter(c for c in hand.cards if c != 'J')
+        sorted_counts = sorted(counts.values(), reverse=True)
+        if len(sorted_counts) == 0:
+            sorted_counts = [5]
+        else:
+            sorted_counts[0] += jokers
+        return Hand.get_rank_with_count(sorted_counts)
+
+    hands.sort(key=lambda h: (get_rank_joker(h).value, list(map(get_card_value_joker, h.cards))))
+
+    total = sum(((key + 1) * hand.bid) for key, hand in enumerate(hands))
+    print(total)
+
+
+# solution_1()
+solution_2()
